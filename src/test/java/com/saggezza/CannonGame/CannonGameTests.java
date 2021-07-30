71d905617e65b80253fb9b8b9c520819470e5c08
Ethan Kane <ethankane92@gmail.com>
2021-07-30 09:44:04 +0100

    Small refactor on test cases

--- src/test/java/com/saggezza/CannonGame/CannonGameTests.java
+++ src/test/java/com/saggezza/CannonGame/CannonGameTests.java
@@ -48,22 +48,22 @@ Flow Class to call methods
 //        When: I create target with an x and y
 
 //        Then: it is created with random X/Y values
-        int[] result = new int[2];
+        int[] result;
         result = target.getRandomXY();
         int size = result.length;
         assertEquals(2, size);
     }
 
     @Test
-    public void testShotValues() {
+    public void testShotValuesAreComputedCorrectly() {
 //        Given: I need a shot
-            int velocity = 5;
-            int angle = 75;
-            IShot shot = new Shot();
-//        When: I create target with an x and y
-            double[] result = new double[2];
+        int velocity = 5;
+        int angle = 75;
+        IShot shot = new Shot();
+//        When: I input a velocity of 5 and an angle of 75
+            double[] result;
             result = shot.computeVelocityAngle(velocity,angle);
-//        Then: it is created with random X/Y values
+//        Then: the inputted angle and velocity are converted into x,y co-ordinates
         double[] calculateResults = new double[2];
             calculateResults[0] =1.2940952255126037;
             calculateResults[1]=4.8296291314453415;
@@ -71,17 +71,17 @@ Flow Class to call methods
             assertArrayEquals(calculateResults,result);
     }
     @Test
-    public void testRoundingShotResult() {
-//        Given: I need a shot
+    public void testShotValuesAreRoundedCorrectlyIntoIntArray() {
+//        Given: I have a shot
         int velocity = 5;
         int angle = 75;
         Shot shot = new Shot();
-//        When: I create target with an x and y
-        double[] result = new double[2];
+//        When: The shot is created with velocity 5 and angle 75
+        double[] result;
         result = shot.computeVelocityAngle(velocity,angle);
-//        Then: it is created with random X/Y values
+//        Then: it is rounded to a whole numbers of type int
         IRoundingShotResult rounding = new RoundingShotResult();
-        int[] actualRoundedResult = new int[2];
+        int[] actualRoundedResult;
         actualRoundedResult = rounding.roundDoubleValues(result);
         int[] roundedResult = new int[2];
         roundedResult[0] = 1;
@@ -91,16 +91,14 @@ Flow Class to call methods
     }
     @Test
     public void testJudgeClassComparesTargetValuesAndRoundedValues() {
-//        Given: I
-        ITarget target = new Target();
-
+//      Given: I need to compare a shot vs a tager
         int velocity = 5;
         int angle = 30;
         Shot shot = new Shot();
-//        When: I create target with an x and y
-        double[] result = new double[2];
+//      When: I pass a [4,2] target and [4,2] shot to the judge
+        double[] result;
         result = shot.computeVelocityAngle(velocity,angle);
-//        Then: it is created with random X/Y values
+//      Then: the target array and shot array are compared and returns true for a match
         IRoundingShotResult rounding = new RoundingShotResult();
         IJudgeClass judge = new JudgeClass();
         System.out.println(Arrays.toString(rounding.roundDoubleValues(result)));
@@ -109,7 +107,6 @@ Flow Class to call methods
         assertEquals(1,judgeResult);
 
     }
-
     /*
     Test counter
     Test Validator - and classes to test valid user input
